72526411b14e1c6bc9207ef7e1e19c6636b608b6
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-07 12:51:14 +0530

    Added: All rules for password

--- BasicInputForm/javaScript/Form.js
+++ BasicInputForm/javaScript/Form.js
@@ -36,10 +36,20 @@ telephone.addEventListener('input', function() {
 const password = document.querySelector('#pwd');
 const passwordError = document.querySelector('.pwd-error');
 password.addEventListener('input', function() {
-    let passwordRegex = RegExp('^[a-zA-Z0-9]{8,}$')
-    if (passwordRegex.test(password.value)) {
+    let passwordRegex = RegExp('^(?=.*[A-z])(?=.*[0-9])(?=.*[@#$%^&*()-+=])([a-zA-Z0-9@._-]).{8,}$')
+    if (passwordRegex.test(password.value))
         passwordError.textContent = "";
-    } else {
+     else
         passwordError.textContent = "Invalid Password";
-    }
-});
+    });
+
+/*
+Rule1 – minimum 8 Characters
+Rule2 – Should have at least 1
+Upper Case
+Rule3 – Should have at least 1
+numeric number in the password
+Rule4 – Has exactly 1 Special
+Character
+- NOTE – All rules must be passed
+*/
