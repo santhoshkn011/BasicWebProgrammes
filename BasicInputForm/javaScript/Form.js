272030e35581391841f3ff54db30d0484f5ebe6f
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-07 12:37:26 +0530

    Added: Telephone Number validation

--- BasicInputForm/javaScript/Form.js
+++ BasicInputForm/javaScript/Form.js
@@ -22,3 +22,13 @@ email.addEventListener('input', function () {
         emailError.textContent = "";
     else emailError.textContent = "Incorrect Email Address.";
     });
+
+const telephone = document.querySelector('#tel');
+const telephoneError = document.querySelector('.tel-error');
+telephone.addEventListener('input', function() {
+        let telRegex = RegExp('^[1-9]{1,2}\\s{1}[0-9]{10}$');
+        if (telRegex.test(telephone.value))
+            telephoneError.textContent = "";
+        else 
+            telephoneError.textContent = "Incorrect Contact Details.";
+    });
